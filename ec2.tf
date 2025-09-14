c720b929cedb9f689b741cd8cd86666f59b31ac1
Avinash <aviroyal2910@gmail.com>
2025-09-14 10:01:02 +0530

    terraform file for basic ec2

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "terra-demo" {
+    ami = "ami-0b982602dbb32c5bd"
+    instance_type = "t3.micro"
+    key_name = "ops"
+
+}
+
+resource "aws_security_group" "demoSG" {
+    
+  
+}
